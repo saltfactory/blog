81e7a6906c277a5f41f84272d3c6579cc7dd0ea4
saltfactory <saltfactory@gmail.com>
2014-11-12 11:59:22 +0900

    add img handler javascript

--- _site/javascripts/main.js
+++ _site/javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+// console.log('This would be the main JS file.');
+
+var imgs = document.getElementsByTagName('img');
+
+for (var i=0; i< imgs.length; i++){
+	var img = imgs[i];
+	var alt = img.alt;
+	
+	var pattern = /({)(.+)(})/g;	
+	var match = pattern.exec(alt);
+	
+	if(match){
+		img.setAttribute("style", match[2]);
+	}
+}
